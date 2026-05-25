c763cc00a2d15b420224c51b53cd184d6120567a
Karl Podger <karl.podger@primeordinal.com>
2026-05-25 12:25:11 +0100

    chore: Populate homepage and prune unused files

--- src/endpoints/seed/viality-marketing.ts
+++ src/endpoints/seed/viality-marketing.ts
@@ -294,7 +294,7 @@ export const seedVialityMarketing = async ({
                   type: 'root',
                   children: [
                     {
-                      type: 'paragraph',
+                       type: 'paragraph',
                       children: [{ type: 'text', text: '' }],
                       direction: 'ltr',
                       format: '',
