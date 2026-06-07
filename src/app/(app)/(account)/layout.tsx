c36c50bdb65b2e0a4dd5a226650aeff6f6fb2bec
Karl Podger <karl.podger@primeordinal.com>
2026-06-07 21:44:42 +0100

    fix: Add margin to checkout page

--- src/app/(app)/(account)/layout.tsx
+++ src/app/(app)/(account)/layout.tsx
@@ -16,7 +16,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
         <RenderParams className="" />
       </div>
 
-      <div className="container mt-16 pb-8 flex gap-8">
+      <div className="container mx-auto mt-16 pb-8 flex gap-8">
         {user && <AccountNav className="max-w-62 grow flex-col items-start gap-4 hidden md:flex" />}
 
         <div className="flex flex-col gap-12 grow">{children}</div>
