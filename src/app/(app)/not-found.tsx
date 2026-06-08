d47390ac048ae2ca7fac8becb3dda2d589ab4a61
Karl Podger <karl.podger@primeordinal.com>
2026-06-08 14:08:20 +0100

    fix: Set horizontal padding of 404 page

--- src/app/(app)/not-found.tsx
+++ src/app/(app)/not-found.tsx
@@ -1,11 +1,10 @@
 import { Link } from "@/components/atoms/Link";
-import React from "react";
 
 import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <div className="container py-28">
+    <div className="container px-4 py-28">
       <div className="prose max-w-none">
         <h1 style={{ marginBottom: 0 }}>404</h1>
         <p className="mb-4">This page could not be found.</p>
